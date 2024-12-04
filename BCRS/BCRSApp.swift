550fe7168b0a4d26206fed1fc9d9cdca6fa75e63
varun-singh26 <149445092+varun-singh26@users.noreply.github.com>
2024-12-03 21:00:13 -0500

    Initial Commit

---
+++ BCRS/BCRSApp.swift
@@ -0,0 +1,17 @@
+//
+//  BCRSApp.swift
+//  BCRS
+//
+//  Created by Varun Singh on 12/3/24.
+//
+
+import SwiftUI
+
+@main
+struct BCRSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
